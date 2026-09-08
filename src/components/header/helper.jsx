9cb25e755c9a25ab88e449d847a03c7e6cc8a175
Aman Vishwakarma <143378283+its-Aman007@users.noreply.github.com>
2026-09-08 15:07:05 +0530

    Update download link

--- src/components/header/helper.jsx
+++ src/components/header/helper.jsx
@@ -4,7 +4,7 @@ const Helper = () => {
   return (
     <div className='cta'>
       <a
-        href='https://1drv.ms/w/c/5a65bf2f422817c4/IQDtMOfuzTk4R7HIUVqDLh0MAW2IVUSk5vaRYQ1RYsKXU_k?e=XSS9UM'
+        href='https://canva.link/0tb6ho0no4nqn4u'
         target='_blank'
         rel='noreferrer'
         className='btn cta-icon dl'
